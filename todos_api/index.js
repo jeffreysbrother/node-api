8c060fadb506783ec49d3d823c45b138db790d5d
James Cool <james.cool@thecontrolgroup.com>
2017-10-12 16:43:53 -0700

    serve static files

--- todos_api/index.js
+++ todos_api/index.js
@@ -7,9 +7,11 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send("HI, FROM THE ROOT ROUTE");
+	res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
