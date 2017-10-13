4249dc578b6bdfdda57e6631dde5f55dc88b72ba
James Cool <james.cool@thecontrolgroup.com>
2017-10-13 13:31:54 -0700

    simplify?

--- todos_api/models/index.js
+++ todos_api/models/index.js
@@ -1,6 +1,23 @@
 let mongoose = require('mongoose');
+
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/todo-api');
-
 mongoose.Promise = Promise;
-module.exports.Todo = require("./todo");
+
+let todoSchema  = new mongoose.Schema({
+	name: {
+		type: String,
+		required: 'Name cannot be blank'
+	},
+	completed: {
+		type: Boolean,
+		default: false
+	},
+	created_date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+let Todo = mongoose.model('Todo', todoSchema);
+module.exports.Todo = Todo;
